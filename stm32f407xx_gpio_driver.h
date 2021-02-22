83b141fb103283ce5d9eae6588055485495dc798
99003512 <78534834+99003512@users.noreply.github.com>
2021-02-22 08:51:04 +0530

    Add files via upload

---
+++ stm32f407xx_gpio_driver.h
@@ -0,0 +1,108 @@
+/*
+ * stm32f407xx_gpio_driver.h
+ *
+ *  Created on: 20-Feb-2021
+ *      Author: Training
+ */
+
+#ifndef INC_STM32F407XX_GPIO_DRIVER_H_
+#define INC_STM32F407XX_GPIO_DRIVER_H_
+#include "stm32f4xx.h"
+
+typedef struct
+{
+	uint8_t GPIO_PinNumber;
+	uint8_t GPIO_PinMode;
+	uint8_t GPIO_PinSpeed;
+	uint8_t GPIO_PinPu;
+	uint8_t GPIO_PinPuPdControl;
+	uint8_t GPIO_PinOPType;
+
+	uint8_t GPIO_PinAltFuncMode;
+
+}GPIO_PinConfig_t;
+
+typedef struct
+{
+GPIO_RegDef_t *pGPIOx;
+GPIO_PinConfig_t GPIO_PinConfig;
+
+}GPIO_HANDLE_t;
+
+#define GPIO_PIN_NO_0    0
+#define GPIO_PIN_NO_1    1
+#define GPIO_PIN_NO_2    2
+#define GPIO_PIN_NO_3    3
+#define GPIO_PIN_NO_4    4
+#define GPIO_PIN_NO_5    5
+#define GPIO_PIN_NO_6    6
+#define GPIO_PIN_NO_7    7
+#define GPIO_PIN_NO_8    8
+#define GPIO_PIN_NO_9    9
+#define GPIO_PIN_NO_10   10
+#define GPIO_PIN_NO_11   11
+#define GPIO_PIN_NO_12   12
+#define GPIO_PIN_NO_13   13
+#define GPIO_PIN_NO_14   14
+#define GPIO_PIN_NO_15   15
+
+
+
+#define GPIO_OP_TYPE_PP		0
+#define GPIO_OP_TYPE_OP		1
+
+//speed of GPIOx
+
+#define GPIO_MODE_IN	     0
+#define GPIO_MODE_OUT        1
+#define GPIO_MODE_ALTFN      2
+#define GPIO_MODE_ANALOG     3
+
+#define GPIO_SPEED_LOW     0
+#define GPIO_SPEED_MEDIUM  1
+#define GPIO_SPEED_FAST    2
+#define GPIO_SPEED_HIGH    3
+
+
+
+#define GPIO_NO_PUPD  		0
+#define GPIO_NO_PU  		1
+#define GPIO_NO_PD  		2
+
+
+
+//to control clock
+void GPIO_PeriClockControl(GPIO_RegDef_t *pGIPOx, uint8_t EnorDi);
+
+
+
+
+void GPIO_init(GPIO_HANDLE_t *pGPIOHandle);
+
+void GPIO_Deinit(GPIO_RegDef_t *pGPIOx);
+uint8_t GPIO_ReadFromInputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber);
+uint16_t GPIO_ReadFromInputPort(GPIO_RegDef_t *pGPIOx);
+
+void GPIO_WriteToOutputPin(GPIO_RegDef_t *pGIPOx, uint8_t PinNumber, uint8_t value);
+void GPIO_WriteToOutputPort(GPIO_RegDef_t *pGIPOx, uint16_t value);
+void GPIO_ToggleOutputPin(GPIO_RegDef_t *pGIPOx, uint8_t PinNumber);
+
+
+
+
+
+
+
+#endif /* INC_STM32F407XX_GPIO_DRIVER_H_ */
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ stm32f4xx.h
@@ -0,0 +1,175 @@
+/*
+ * stm32f4xx.h
+ *
+ *  Created on: 20-Feb-2021
+ *      Author: Training
+ */
+
+#ifndef INC_STM32F4XX_H_
+#define INC_STM32F4XX_H_
+
+
+
+#include <stdint.h>
+
+//defining memory definition
+
+#define SRAM1_BASE_ADDR 0x20000000U
+#define SRAM2_BASE_ADDR 0x2001C000U
+#define FLASH_BASE_ADDR 0x08000000U
+
+#define ROM_BASE_ADDR  0x1FFF0000U
+#define __vo volatile
+//defining peripheral base addresses
+
+#define APB1_BASEADDR  0x40000000U
+#define APB2_BASEADDR  0x40010000U
+#define AHB1_BASEADDR  0x40020000U
+#define RCC_BASEADDR   (AHB1_BASEADDR + 0x3800)
+//defining the peripherals hanging onto ahb1 bus
+
+#define GPIOA_BASEADDR  (AHB1_BASEADDR + 0x000U)
+#define GPIOB_BASEADDR  (AHB1_BASEADDR + 0x0400U)
+#define GPIOC_BASEADDR  (AHB1_BASEADDR + 0x0800U)
+#define GPIOD_BASEADDR  (AHB1_BASEADDR + 0x0C00U)
+#define GPIOE_BASEADDR  (AHB1_BASEADDR + 0x1000U)
+#define GPIOF_BASEADDR  (AHB1_BASEADDR + 0X1400U)
+#define GPIOG_BASEADDR  (AHB1_BASEADDR + 0X1800U)
+#define GPIOH_BASEADDR  (AHB1_BASEADDR + 0X1C00U)
+#define GPIOI_BASEADDR  (AHB1_BASEADDR + 0X2000U)
+#define GPIOJ_BASEADDR  (AHB1_BASEADDR + 0X2400U)
+#define GPIOK_BASEADDR  (AHB1_BASEADDR + 0X2800U)
+
+
+
+#define TIM2_BASEADDR (APB1_BASEADDR)
+
+#define TIM1_BASEADDR (APB2_BASEADDR)
+
+
+typedef struct{
+	__vo uint32_t MODER;
+	__vo uint32_t OTYPER;
+	__vo uint32_t OSPEEDR;
+	__vo uint32_t PUPDR;
+	__vo uint32_t IDR;
+	__vo uint32_t ODR;
+	__vo uint32_t BSRR;
+	__vo uint32_t LCKR;
+	__vo uint32_t AFR[2];
+
+} GPIO_RegDef_t;
+
+#define GPIOA ((GPIO_RegDef_t*)GPIOA_BASEADDR)
+#define GPIOB ((GPIO_RegDef_t*)GPIOB_BASEADDR)
+#define GPIOC ((GPIO_RegDef_t*)GPIOC_BASEADDR)
+#define GPIOD ((GPIO_RegDef_t*)GPIOD_BASEADDR)
+#define GPIOE ((GPIO_RegDef_t*)GPIOE_BASEADDR)
+#define GPIOF ((GPIO_RegDef_t*)GPIOF_BASEADDR)
+#define GPIOG ((GPIO_RegDef_t*)GPIOG_BASEADDR)
+#define GPIOH ((GPIO_RegDef_t*)GPIOH_BASEADDR)
+#define GPIOI ((GPIO_RegDef_t*)GPIOI_BASEADDR)
+
+
+typedef struct{
+		__vo uint32_t CR;
+		__vo uint32_t PLLCFGR;
+		__vo uint32_t CFGR;
+		__vo uint32_t CIR;
+		__vo uint32_t AHB1RSTR;
+		__vo uint32_t AHB2RSTR;
+		__vo uint32_t AHB3RSTR;
+			 uint32_t Reserved;
+		__vo uint32_t APB1RSTR;
+		__vo uint32_t APB2RSTR;
+			 uint32_t Reserved1[2];
+		__vo uint32_t AHB1ENR;
+		__vo uint32_t AHB2ENR;
+		__vo uint32_t AHB3ENR;
+			 uint32_t Reserved2;
+		__vo uint32_t APB1ENR;
+		__vo uint32_t APB2ENR;
+			 uint32_t Reserved3[2];
+		__vo uint32_t AHB1LPENR;
+		__vo uint32_t AHB2LPENR;
+		__vo uint32_t AHB3LPENR;
+		__vo uint32_t Reserved4;
+		__vo uint32_t APB1LPENR;
+		__vo uint32_t APB2LPENR;
+			 uint32_t Reserved5[2];
+		__vo uint32_t BDCR;
+		__vo uint32_t CSR;
+			 uint32_t Reserved6[2];
+		__vo uint32_t SSCGR;
+		__vo uint32_t PLLI2SCFGR;
+
+
+} RCC_RegDef_t;
+
+#define RCC  			((RCC_RegDef_t*)RCC_BASEADDR)
+
+//Setting the clock
+
+#define GPIOA_PCLK_EN()    (RCC -> AHB1ENR |= (1<<0))
+#define GPIOB_PCLK_EN()    (RCC -> AHB1ENR |= (1<<1))
+#define GPIOC_PCLK_EN()    (RCC -> AHB1ENR |= (1<<2))
+#define GPIOD_PCLK_EN()    (RCC -> AHB1ENR |= (1<<3))
+#define GPIOE_PCLK_EN()    (RCC -> AHB1ENR |= (1<<4))
+#define GPIOF_PCLK_EN()    (RCC -> AHB1ENR |= (1<<5))
+#define GPIOG_PCLK_EN()    (RCC -> AHB1ENR |= (1<<6))
+#define GPIOH_PCLK_EN()    (RCC -> AHB1ENR |= (1<<3))
+#define GPIOI_PCLK_EN()    (RCC -> AHB1ENR |= (1<<3))
+//resetting
+
+
+
+#define GPIOA_PCLK_DIS()  (RCC -> AHB1ENR &= ~(1 << 0))
+#define GPIOB_PCLK_DIS()  (RCC -> AHB1ENR &= ~(1 << 1))
+#define GPIOC_PCLK_DIS()  (RCC -> AHB1ENR &= ~(1 << 2))
+#define GPIOD_PCLK_DIS()  (RCC -> AHB1ENR &= ~(1 << 3))
+#define GPIOE_PCLK_DIS()  (RCC -> AHB1ENR &= ~(1 << 4))
+
+
+
+#define GPIOA_REG_RESET()		do{(RCC->AHB1RSTR |= (1<<0)); (RCC->AHB1RSTR &= ~(1<<0)); }while(0)
+#define GPIOB_REG_RESET()		do{(RCC->AHB1RSTR |= (1<<1)); (RCC->AHB1RSTR &= ~(1<<1)); }while(0)
+#define GPIOC_REG_RESET()		do{(RCC->AHB1RSTR |= (1<<2)); (RCC->AHB1RSTR &= ~(1<<2)); }while(0)
+#define GPIOD_REG_RESET()		do{(RCC->AHB1RSTR |= (1<<3)); (RCC->AHB1RSTR &= ~(1<<3)); }while(0)
+#define GPIOE_REG_RESET()		do{(RCC->AHB1RSTR |= (1<<4)); (RCC->AHB1RSTR &= ~(1<<4)); }while(0)
+#define GPIOF_REG_RESET()		do{(RCC->AHB1RSTR |= (1<<5)); (RCC->AHB1RSTR &= ~(1<<5)); }while(0)
+#define GPIOG_REG_RESET()		do{(RCC->AHB1RSTR |= (1<<6)); (RCC->AHB1RSTR &= ~(1<<6)); }while(0)
+#define GPIOH_REG_RESET()		do{(RCC->AHB1RSTR |= (1<<7)); (RCC->AHB1RSTR &= ~(1<<7)); }while(0)
+#define GPIOI_REG_RESET()		do{(RCC->AHB1RSTR |= (1<<8)); (RCC->AHB1RSTR &= ~(1<<8)); }while(0)
+
+
+
+
+
+
+
+
+
+
+#define ENABLE    1
+#define DISABLE    0
+#define SET      ENABLE
+#define RESET     DISABLE
+#define GPIO_PIN_SET	SET
+
+#define GPIO_PIN_RESET  RESET
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* INC_STM32F4XX_H_ */
