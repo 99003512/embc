2816fafb1f32c7f55240b3a4520ffc879f187443
99003512 <78534834+99003512@users.noreply.github.com>
2021-02-24 18:27:45 +0530

    Add files via upload

---
+++ Startup/project1/Debug/drivers/Src/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../drivers/Src/gpio_driver.c \
+../drivers/Src/stm32f407xx_gpio_driver.c 
+
+OBJS += \
+./drivers/Src/gpio_driver.o \
+./drivers/Src/stm32f407xx_gpio_driver.o 
+
+C_DEPS += \
+./drivers/Src/gpio_driver.d \
+./drivers/Src/stm32f407xx_gpio_driver.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+drivers/Src/gpio_driver.o: ../drivers/Src/gpio_driver.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F407G_DISC1 -DSTM32F4 -DSTM32F407VGTx -DDEBUG -c -I../Inc -I"D:/STM32project/project1/drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"drivers/Src/gpio_driver.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+drivers/Src/stm32f407xx_gpio_driver.o: ../drivers/Src/stm32f407xx_gpio_driver.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F407G_DISC1 -DSTM32F4 -DSTM32F407VGTx -DDEBUG -c -I../Inc -I"D:/STM32project/project1/drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"drivers/Src/stm32f407xx_gpio_driver.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
